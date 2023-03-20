e0c47e04d61e6d2f0e7e444c51a854c4a2e10756
sufyaniitk <126153355+sufyaniitk@users.noreply.github.com>
2023-03-21 02:41:22 +0530

    Delete p9.py

--- p9.py
+++
@@ -1,8 +0,0 @@
-from matplotlib import pyplot as plt
-import control
-
-s = control.TransferFunction.s
-# Write the value of G(s)H(s)
-G = 1/((s+1)*(s+2)*(s+3))
-rlist, klist = control.rlocus(G)
-plt.show()
